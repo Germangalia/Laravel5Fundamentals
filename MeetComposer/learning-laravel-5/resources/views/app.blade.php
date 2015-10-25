81a4f1576c1995bb4cff66160ddcfe6ed0ff8f18
Germangalia <germangalia@iesebre.com>
2015-10-25 14:13:29 +0100

    Video 5 laracast versió 1

---
+++ MeetComposer/learning-laravel-5/resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div class="container">
+         @yield('content')
+    </div>
+</body>
+</html>
--- MeetComposer/learning-laravel-5/resources/views/pages/about.blade.php
+++ MeetComposer/learning-laravel-5/resources/views/pages/about.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>About</title>
+@extends('app')
 
-</head>
-<body>
-<h1>About me: {{$first}} {{$last}}</h1>
-<p>Lorem ipsum...</p>
-</body>
-</html>
+@section('content')
+    <h1>About me: {{$first}}</h1>
+    <p>Lorem ipsum...</p>
+
+@stop
